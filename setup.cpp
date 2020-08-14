c76544433ed5c2a819f522fd2aa00263563b1e11
diyanqi <60802943+diyanqi@users.noreply.github.com>
2020-08-14 13:31:52 +0800

    Add files via upload

---
+++ setup.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+system("apt-get install gcc");
+system("apt-get install gcc --fix-missing");
+system("apt-get install python3");
+system("apt-get install python3 --fix-missing");
+system("apt-get install git");
+system("apt-get install git --fix-missing");
+system("git clone https://github.com/diyanqi/TastickOJ/");
+system("apt install python3-pip");
+system("apt-get install python3-pip --fix-missing");
+system("pip3 install timeout_decorator -i https://pypi.tuna.tsinghua.edu.cn/simple");
+system("pip3 install flask -i https://pypi.tuna.tsinghua.edu.cn/simple");
+system("pip3 install flask_cors -i https://pypi.tuna.tsinghua.edu.cn/simple");
+system("pip3 install json -i https://pypi.tuna.tsinghua.edu.cn/simple");
+system("pip3 install datetime -i https://pypi.tuna.tsinghua.edu.cn/simple");
+system("pip3 install difflib");
+system("pip3 install shutil");
+system("pip3 install glob");
+system("python3 TastickOJ/main.py");
+return 0;
+}
